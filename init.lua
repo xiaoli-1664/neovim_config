2a0357fa88a435494f92382448d0780d772b06ea
xiaoli-1664 <384520024@qq.com>
2025-11-01 11:44:48 +0800

    latex

--- init.lua
+++ init.lua
@@ -622,7 +622,7 @@ require('lazy').setup({
             --
             --  Add any additional override configuration in the following tables. Available keys are:
             --  - cmd (table): Override the default command used to start the server
-            --  - filetypes (table): Override the default list of associated filetypes for the server
+            --  - fletypes (table): Override the default list of associated filetypes for the server
             --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
